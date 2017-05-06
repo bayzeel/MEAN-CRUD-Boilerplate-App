20061588c8980ad9aa0b4188a6da97ea90cc5716
Basil Sklepkovich <vsklepkovich@gmail.com>
2017-05-06 16:50:48 +0300

    Create adding form with validation

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators, AbstractControl} from "@angular/forms";
+//import "rxjs/Rx";
+//import "rxjx/add/operator/debounceTime";
+//import "rxjx/add/operator/distingUntilChanged";
 
 @Component({
   selector: 'app-add',
@@ -6,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+  addForm: FormGroup;
+  firstNameCtrl: FormControl;
+  lastNameCtrl: FormControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.firstNameCtrl = fb.control("", [Validators.required]);
+    this.lastNameCtrl = fb.control("", [Validators.required]);
+    this.addForm = fb.group({
+      firstName: this.firstNameCtrl,
+      lastName: this.lastNameCtrl
+    });
+  }
 
   ngOnInit() {
   }
